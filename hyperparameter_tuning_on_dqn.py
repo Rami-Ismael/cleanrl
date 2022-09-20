c7f99d7d3fe426b10e3f0c6789378efbe6184030
Rami-Ismael <rami.m.ismael@gmail.com>
2022-09-20 13:35:38 -0500

    remove hyperparameter that only exist in dqn

--- hyperparameter_tuning_on_dqn.py
+++ hyperparameter_tuning_on_dqn.py
@@ -14,11 +14,6 @@ tuner = Tuner(
     },
     params_fn=lambda trial: {
         "learning-rate": trial.suggest_loguniform("learning-rate", 0.0003, 0.003),
-        "num-minibatches": trial.suggest_categorical("num-minibatches", [1, 2, 4]),
-        "update-epochs": trial.suggest_categorical("update-epochs", [1, 2, 4, 8]),
-        "num-steps": trial.suggest_categorical("num-steps", [5, 16, 32, 64, 128]),
-        "vf-coef": trial.suggest_uniform("vf-coef", 0, 5),
-        "max-grad-norm": trial.suggest_uniform("max-grad-norm", 0, 5),
         "total-timesteps": 100000,
         "num-envs": 16,
         "torch-deterministic": True,
@@ -26,6 +21,9 @@ tuner = Tuner(
         "gamma": trial.suggest_uniform("gamma", 0.9, 0.999),
         "batch-size": trial.suggest_int("batch-size", 128, 256),
         "quantize": trial.suggest_categorical("quantized", [True, False]),
+        "target-network-frequency": trial.suggest_int("target-network-frequency", 100 , 1000),
+        "start-e": 1,
+        "end-e": 0.05,
         "cuda": True
     },
     pruner=optuna.pruners.MedianPruner(n_startup_trials=5),
