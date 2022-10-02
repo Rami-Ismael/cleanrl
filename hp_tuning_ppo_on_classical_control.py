7d559ec1ca9bb9da8bf951f37a78d1a6d3037e2d
Rami-Ismael <rami.m.ismael@gmail.com>
2022-10-02 07:04:07 -0500

    remove changing the batch and mini batches while optimizing the problems

--- hp_tuning_ppo_on_classical_control.py
+++ hp_tuning_ppo_on_classical_control.py
@@ -24,12 +24,10 @@ for env in env_list:
             "cuda": True,
             "total-timesteps": 500000,
             "learning-rate": trial.suggest_uniform("learning-rate", 2.5e-5, 0.1),
-            "num-step": trial.suggest_int("num-step", 16 , 1024),
             "anneal-lr": trial.suggest_categorical("annealing-lr", [True, False]),
             "gae": trial.suggest_categorical("gae", [True, False]),
             "gamma": trial.suggest_uniform("gamma", 0.1, 0.999),\
             "gae-lambda": trial.suggest_uniform("gae-lambda", 0.1, 0.999),
-            "num-minibatches": trial.suggest_int("num-mini-batch", 2, 1024),
             "update-epochs": trial.suggest_int("update-epochs", 1, 1024),
             "norm-adv": trial.suggest_categorical("norm-adv", [True, False]),
             "clip-coef": trial.suggest_uniform("clip-coef", 0.1, 0.999),
