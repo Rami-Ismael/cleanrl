551450c184b42d7ac2d56d0c6bf28b52e5c119ea
Rami-Ismael <rami.m.ismael@gmail.com>
2022-12-18 13:11:30 -0600

    Create a new model experiemtn files that will create dqn model and upload to hugging face hub

---
+++ experiment/dqn-classic-control-no-sync.py
@@ -0,0 +1,32 @@
+from random import random
+import sys
+
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '../cleanrl')
+from cleanrl.functional.no_sync.functional_dqn import dqn_functional
+
+
+'''
+This code gaols it to train a DQN agent on the 
+dicrete classic control environments and upload to hugging Face
+'''
+
+dicrete_box_envs = [ "CartPole-v1" , "MountainCar-v0" , "Acrobot-v1" ]
+
+optimizers = [ "Adam" , "Adan" , "hAdam" ]
+
+
+for env_id in dicrete_box_envs:
+    for optimizer in optimizers:
+        for seed in range( 0 , 3):
+            output = dqn_functional(
+                seed = int(seed),
+                env_id=env_id,
+                track = True,
+                optimizer = optimizer , 
+                quantize_weight = False , 
+                quantize_activation = False,
+                wandb_entity = "compress_rl" , 
+                wandb_project="cleanrl",
+                trial = None
+            )
